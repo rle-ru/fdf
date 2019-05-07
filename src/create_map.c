698bda42b585baee9418aacb0c366b480e9188d1
Rodolphe LE RU <rle-ru@e2r4p4.42.fr>
2019-05-07 13:23:18 +0200

    removed useless vars

--- src/create_map.c
+++ src/create_map.c
@@ -6,7 +6,7 @@
 /*   By: rle-ru <rle-ru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 18:06:09 by rle-ru            #+#    #+#             */
-/*   Updated: 2019/05/07 13:02:02 by rle-ru           ###   ########.fr       */
+/*   Updated: 2019/05/07 13:23:07 by rle-ru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,32 @@
 
 static void		ft_split_line(t_fdf *fdf, int i, t_line *line)
 {
-	int		j;
 	int		k;
 	int		lpos;
 
 	k = 0;
 	lpos = 0;
-	j = i * fdf->lines->nbx;
+	i *= fdf->lines->nbx;
 	while (k < line->nbx)
 	{
 		while (line->line[lpos] && line->line[lpos] == ' ')
 			++lpos;
-		fdf->map[j] = ft_atoi(line->line + lpos);
+		fdf->map[i] = ft_atoi(line->line + lpos);
 		while (line->line[lpos]
 				&& (ft_isdigit(line->line[lpos])
 					|| line->line[lpos] == '-'))
 			++lpos;
 		++k;
-		++j;
+		++i;
 	}
 }
 
 t_error			ft_create_map(t_fdf *fdf)
 {
 	int		i;
-	int		j;
 	t_line	*line;
 
 	i = 0;
-	j = 0;
 	if (!(fdf->map = malloc(sizeof(int) * fdf->nblines * fdf->lines->nbx)))
 		return (falloc);
 	line = fdf->lines;
@@ -53,6 +50,5 @@ t_error			ft_create_map(t_fdf *fdf)
 		++i;
 		line = line->next;
 	}
-	i = 0;
 	return (ok);
 }
