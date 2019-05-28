a9a156936b25a1c6cb85c5b790275ccbe9d1e1c7
rle-ru <rle-ru@student.42.fr>
2019-05-28 06:54:18 +0200

    /dev/zero

--- src/open_file.c
+++ src/open_file.c
@@ -6,17 +6,20 @@
 /*   By: rle-ru <rle-ru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 11:53:02 by rle-ru            #+#    #+#             */
-/*   Updated: 2019/05/15 22:17:32 by rle-ru           ###   ########.fr       */
+/*   Updated: 2019/05/28 06:52:01 by rle-ru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "fdf.h"
+#include "libft.h"
 
 t_error	ft_open_file(int ac, char **av, t_fdf *fdf)
 {
 	if (ac == 1)
 		return (usage);
+	if (!ft_strcmp(av[1], "/dev/zero"))
+		return (filepath);
 	if ((fdf->parser.fd = open(av[1], O_RDONLY)) == -1)
 	{
 		fdf->parser.fname = av[1];
